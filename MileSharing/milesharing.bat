1fbcaf7904b92c0add8ae5c1613307a1468e4ee5
pens <litvinenko.htc@gmail.com>
2014-08-23 17:17:35 +0300

    fix key

---
+++ MileSharing/milesharing.bat
@@ -0,0 +1,3 @@
+D:\android\sdk\platform-tools\adb.exe install -r D:\android\myWorkspace\MileSharing\MileSharing\MileSharing.apk
+D:\android\sdk\platform-tools\adb.exe shell am start -n com.soloway.transport.milesharing/com.soloway.city.milesharing.SplashActivity
+pause
