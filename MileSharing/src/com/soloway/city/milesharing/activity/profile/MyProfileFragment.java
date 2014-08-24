e4dba806e724032a27085a544f432f599e4154fa
pens <litvinenko.htc@gmail.com>
2014-08-24 08:28:25 +0300

    add custom info window and autozoom

---
+++ MileSharing/src/com/soloway/city/milesharing/activity/profile/MyProfileFragment.java
@@ -0,0 +1,7 @@
+package com.soloway.city.milesharing.activity.profile;
+
+/**
+ * Created by pens on 24.08.14.
+ */
+public class MyProfileFragment {
+}
---
+++ MileSharing/src/com/soloway/city/milesharing/activity/rout/RoutFragment.java
@@ -0,0 +1,9 @@
+package com.soloway.city.milesharing.activity.rout;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by pens on 24.08.14.
+ */
+public class RoutFragment extends Fragment {
+}
